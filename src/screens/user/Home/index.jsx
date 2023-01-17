5030a0c3903fe132fa78fde7a4b24c24198ae235
Hamzakh3 <hamkh3@gmail.com>
2023-01-17 14:26:42 +0500

    Configure Store and Slice

--- src/screens/user/Home/index.jsx
+++ src/screens/user/Home/index.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment, incrementByAmount } from '../../../store/slices/counterSlice'
 
 function Home() {
+  const dummy = useSelector(state => state)
+  console.log(dummy);
+  const dispatch = useDispatch()
   return (
-    <div>Home</div>
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{dummy.counter.value}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+        
+        <button
+          aria-label="Increment by amount"
+          onClick={() => dispatch(incrementByAmount(3))}
+        >
+          Increment By Amount
+        </button>
+      </div>
+    </div>
   )
 }
 
---
+++ src/store/slices/counterSlice.js
@@ -0,0 +1,39 @@
+import { createAction, createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    value: 0,
+    name: "Hamza"
+}
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment: (state) => {
+            // Redux Toolkit allows us to write "mutating" logic in reducers. It
+            // doesn't actually mutate the state because it uses the Immer library,
+            // which detects changes to a "draft state" and produces a brand new
+            // immutable state based off those changes
+
+            state.value += 1
+            
+        },
+        decrement: (state) => {
+            state.value -= 1
+            
+        },
+        incrementByAmount: (state, action) => {
+            state.value += action.payload
+            
+        },
+    },
+    extraReducers: builder => {
+        builder.addCase(createAction("inc"), (state, action) => {
+            state.value += 1
+        })
+    }
+
+})
+
+export const {increment, decrement, incrementByAmount} = counterSlice.actions
+export default counterSlice.reducer

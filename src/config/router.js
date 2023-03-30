a5a79190e69f3741cf6605598ef4e41230d1d09b
Hamzakh3 <hamkh3@gmail.com>
2023-03-31 01:50:40 +0500

    Update Router.js file, Create Register page, Login and register pages shift to authenticate folder

--- src/config/router.js
+++ src/config/router.js
@@ -4,24 +4,26 @@ import {
     Routes, Route
 } from "react-router-dom";
 
-// import Home from '../screens/user/Home';
-// import UserProfile from '../screens/user/UserProfile';
-import LiveClasses from 'src/screens/user/LiveClasses';
-// import VideoClasses from '../screens/user/VideoClasses';
-import Login from "src/screens/user/Login";
 import Header from 'src/components/Header';
 import Footer from 'src/components/Footer';
-// <<<<<<<
-//  HEAD
+
+// Authenticate
+import Login from "src/screens/Authenticate/Login";
+import Register from "src/screens/Authenticate/Register";
+
+// User
+import LiveClasses from 'src/screens/user/LiveClasses';
+import Instructor from 'src/screens/user/Instructor'
+import SingleInstructor from "src/screens/user/SingleInstructor";
+
+// User Dashboard
+import History from "src/screens/user/Dashboard/Screen/HistoryScreen/Index";
 import MembershipScreen2 from "src/screens/user/Dashboard/Screen/MembershipScreen2/MembershipScreen2";
 import Membership from "src/screens/user/Dashboard/Screen/MembershipScreen/Index";
 import MyProfile from "src/screens/user/Dashboard/Screen/MyProfileScreen/Index";
 import Payment from "src/screens/user/Dashboard/Screen/PaymentScreen/Index";
-
-import Instructor from 'src/screens/user/Instructor'
-import SingleInstructor from "src/screens/user/SingleInstructor";
-import History from "src/screens/user/Dashboard/Screen/HistoryScreen/Index";
 import LiveBooking from "src/screens/user/Dashboard/Screen/LiveBookingScreen/Index";
+
 
 //f0d18eebe6a4a8805d27a3031a904dcb344de975
 
@@ -52,44 +54,35 @@ export default function Router() {
     //     // Calling a auth function if user logged in setUser with id or something. Else setUser to null.
     // }, [])
 
-    return <div>
+    return (
+      <div>
         <BrowserRouter>
-            {/* {user ? <DashboardHeader /> : <Header />} */}
-            <Header />
+          {/* {user ? <DashboardHeader /> : <Header />} */}
+          <Header />
 
-            <Routes>
-                {/* <Route path="/" element={<Home />} /> */}
-                <Route path="/" element={<LiveClasses />} />
-                {/* <Route path="/live-classes" element={<LiveClasses />} /> */}
-                {/* <Route path="/video-classes" element={<VideoClasses />} />
-                <Route path="/user-profile" element={<UserProfile />} />*/
-                    <Route path="/login" element={<Login />} />
-                /* <Route path="/" element={protectedAuthRoute(<Login />)} />
-                <Route path="/login" element={protectedAuthRoute(<Login />)} />
-                <Route path="/signup" element={protectedAuthRoute(<Signup />)} />
-                
-                <Route path="/dashboard" element={protectedRoute(<Dashboard />)} />
-                <Route path="/createAd" element={protectedRoute(<CreateAd />)} />
-<<<<<<< HEAD
-            <Route path="/productDetails/:adId" element={protectedRoute(<ProductDetails />)} /> */}
+          <Routes>
+            {/* Authenticate */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
 
-=======
-                {/* <Route path="/productDetails/:adId" element={protectedRoute(<ProductDetails />)} />  */}
-                {<Route path="/instructor" element={<Instructor />} />}
-                {<Route path="/singleinstructor/:id" element={<SingleInstructor />} />}
-                <Route path="/membership" element={<Membership/>}/>
-                <Route path="/myprofile" element={<MyProfile/>}/>
-                <Route path="/membership2" element={<MembershipScreen2/>}/>
-                <Route path="/payment" element={<Payment/>}/>
-                <Route path="/history" element={<History/>}/>
-                <Route path="/liveBooking" element={<LiveBooking/>}/>
+            {/* User */}
+            <Route path="/" element={<LiveClasses />} />
+            <Route path="/instructor" element={<Instructor />} />
+            <Route
+              path="/singleinstructor/:id"
+              element={<SingleInstructor />}
+            />
 
-            </Routes>
-            <Footer />
-
+            {/* User Dashboard */}
+            <Route path="/membership" element={<Membership />} />
+            <Route path="/myprofile" element={<MyProfile />} />
+            <Route path="/membership2" element={<MembershipScreen2 />} />
+            <Route path="/payment" element={<Payment />} />
+            <Route path="/history" element={<History />} />
+            <Route path="/liveBooking" element={<LiveBooking />} />
+          </Routes>
+          <Footer />
         </BrowserRouter>
-    </div>
+      </div>
+    );
 }
-
-//Types of Routing
-//1. 
---
+++ src/screens/Authenticate/Register/content.js
@@ -0,0 +1,13 @@
+export const regsiterContent = {
+  signUp: "SIGN UP",
+  signUpLine: "Sign up to access all of your content",
+  name: "Name",
+  namePlaceholder: "Enter name",
+  email: "E-mail",
+  emailPlaceHolder: "Enter E-mail address",
+  password: "Password",
+  confirmPassword: "Confirm Password",
+  btnSignup: "Sign up",
+  ifYouHave: "Already have an account ? ",
+  signIn: "Sign in",
+};

a84538a8d0a9f410938adb8527517df19ceabc37
Hamzakh3 <hamkh3@gmail.com>
2023-07-05 16:09:31 +0500

    working on  history cards

--- src/components/Cards/HistoryCard.jsx
+++ src/components/Cards/HistoryCard.jsx
@@ -1,16 +1,72 @@
 import React from "react";
-import { H3,P3 } from "src/components";
-import { Card, CardContent } from "./cardsComponent";
+import { H3, P3, H4 } from "src/components";
 import { icons } from "src/helpers";
 import { layout } from "src/helpers";
-import styled from "styled-components"
+import styled from "styled-components";
 import { HorizontalLine2 } from "src/screens/user/Dashboard/Screen/MyProfileScreen/ProfileComponent";
+import IntensityLevel from "../IntensityLevel";
 
-const { mobile, tablet} = layout;
+const { mobile, tablet } = layout;
+const Card = styled.div`
+  max-width: 982px;
+  display: flex;
+  align-items: center;
+  gap: 104px;
+`;
+const ImageBox = styled.div`
+  position: relative;
+  img {
+    /* max-width: 334px; */
+    aspect-ratio: 1.65/1;
+    object-fit: cover;
+  }
+  .label {
+    background: var(--backgroundGreen);
+    padding: 4px 16px;
+    color: #fff;
+    position: absolute;
+    left: 0px;
+  }
+`;
+
+const CardContent = styled.div`
+  flex: 1;
+  .timeRow {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+    .time {
+      display: flex;
+      align-items: center;
+      gap: 8px;
+    }
+  }
+
+  .intensityRow {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 16px;
+    margin-bottom: 20px;
+    .instrutor {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      gap: 4px;
+    }
+  }
+
+  .detail{
+    max-width: 482px;
+  }
+`;
+
 export function HistoryCard({ img }) {
   return (
     <>
-    <Card style={{ display: "flex", justifyContent:"space-between",flexWrap:"wrap"}}>
+      {/* <Card style={{ display: "flex", justifyContent:"space-between",flexWrap:"wrap"}}>
       <CardImg>
         <img src={img} alt="workout" />
       </CardImg>
@@ -90,66 +146,37 @@ export function HistoryCard({ img }) {
           </P3>
         </div>
       </CardText>
-    </Card>
-    <HorizontalLine2 style={{marginBottom:"20px"}}/>
-    </>
+    </Card> */}
+      <Card>
+        <ImageBox>
+          <img src={img} alt="Card Image" width="" height="" />
+          <span className="label">Yoga</span>
+        </ImageBox>
+        <CardContent>
+          <div className="timeRow">
+            <H3>UPPER BODY POWER</H3>
+            <div className="time">
+              <img src={icons.timeClock} alt="" width="15px" height="auto" />
+              <P3>5 min 47 sec</P3>
+            </div>
+          </div>
 
+          <div className="intensityRow">
+            <div className="instrutor">
+              <img src={icons.shortpicWomen} alt="img" />
+              <p4>Elizebeth Lisa</p4>
+            </div>
+            <IntensityLevel level={3} />
+          </div>
+
+          <P3 className="detail">
+            Lorem ipsum dolor sit amet consectetur. Eget vulputate sed posuere
+            sit. Integer in ac sem adipiscing nulla arcu Enim placerat nunc
+            tincidunt gravida vitae tincidunt
+          </P3>
+        </CardContent>
+      </Card>
+      <HorizontalLine2 style={{ marginBlock: "20px" }} />
+    </>
   );
 }
-export const CardImg = styled.div`
-  position: relative;
-  margin-bottom: 19px;
-
-
-  @media only screen and (min-width: ${mobile}) {
-    img {
-      max-height: 100px;
-      object-fit: cover;
-      object-position: center;
-    }
-  }
-  @media only screen and (min-width: ${tablet}) {
-    img {
-      max-height: initial;
-      object-fit: initial;
-      object-position: initial;
-      
-    }
-  }
-  `
-
-export const CardText = styled(CardContent)`
-max-width:505px;
-min-width:100px;
-padding:20px 0px 20px 0px;
-gap:2rem
-.x {
-      transform: translateY(0.5rem);
-    }
-
-
- .flex{
-  display:flex;
-  gap :5px;
-}
-.intensityInstructor{
-  padding-Bottom:20px;
-  gap:0px;
-  justify-content:space-between;
-  flex-wrap:wrap;
-}
-.f{
-  display:flex;
-  justify-content:space-between;
-align-items:center;
-gap :10px;
-}
-.timeRow{
-  display:flex;
-  flex-wrap:wrap;
-  padding-bottom:20px
-  min-width:100px
-  justify-content:center;
-}
-`;
-
--- src/screens/user/Dashboard/Screen/LiveBookingScreen/Index.jsx
+++ src/screens/user/Dashboard/Screen/LiveBookingScreen/Index.jsx
@@ -1,27 +1,13 @@
 import React from 'react'
-import Sidebar from '../../Components/Sidebar/Index'
-import { Container } from '../../Components/Container'
-import { Heading,Heading2 } from '../../Components/Heading'
-import { Wrapper } from '../../Components/Wrapper'
-import { HorizontalLine2 } from '../MyProfileScreen/ProfileComponent'
+import { Heading2 } from '../../Components/Heading'
 import { bookingScreenStaticContent as content } from "./Constant";
 
 const LiveBooking = () => {
   return (
-    <Wrapper>
-       <div className="membership_top_div">
-
-    <Heading>{content.bookingH1}</Heading>
-    <HorizontalLine2 />
-    </div>
-    <div className="whole_box">
-      <Sidebar />
-      <Container >
+    <div>
       <Heading2>{content.bookingH2}</Heading2>
-      </Container>
     </div>
-  </Wrapper>
-  )
+  );
 }
 
 export default LiveBooking
